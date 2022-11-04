d2662f3dac8674290350ea76c7a7c5da7e79fba3
teneko <giteroneko@gmail.com>
2022-11-04 10:01:30 +0100

    test: added tests for encoder to test null values

--- test/PseudoDynamic.Terraform.Plugin.Sdk.Test/Sdk/Transcoding/TerraformDynamicMessagePackEncoderGenericsTests.cs
+++ test/PseudoDynamic.Terraform.Plugin.Sdk.Test/Sdk/Transcoding/TerraformDynamicMessagePackEncoderGenericsTests.cs
@@ -16,12 +16,28 @@ namespace PseudoDynamic.Terraform.Plugin.Sdk.Transcoding
         }
 
         [Fact]
+        internal void Generic_encode_value_can_handle_null()
+        {
+            var encoder = new ContentTypeEvaluatingEncoder();
+            encoder.EncodeValue<object?>(DynamicDefinition.Uncomputed(), default);
+            encoder.ContentType.Should().Be(typeof(object));
+        }
+
+        [Fact]
         internal void Non_generic_encode_value_inferres_correct_content_type()
         {
             var encoder = new ContentTypeEvaluatingEncoder();
             var number = 2;
             encoder.EncodeValue(PrimitiveDefinition.Number, (object?)number);
             encoder.ContentType.Should().Be(typeof(int));
+        }
+
+        [Fact]
+        internal void Non_generic_encode_value_can_handle_null()
+        {
+            var encoder = new ContentTypeEvaluatingEncoder();
+            encoder.EncodeValue(DynamicDefinition.Uncomputed(), default);
+            encoder.ContentType.Should().Be(typeof(object));
         }
 
         class ContentTypeEvaluatingEncoder : TerraformDynamicMessagePackEncoder
